4ada261147df7a523e4fab511f7ba9042c830743
Wanjirufay <wa.njirufay401@gmail.com>
2025-03-10 19:13:06 +0300

    this was simple

---
+++ test.js
@@ -0,0 +1,48 @@
+/**
+* Problem: Create a functions that shows sum of two numbers
+*/
+
+
+const addNumbers = (a, b) => {
+    return (a + b);
+   };
+   
+   
+   const a =10;
+   const b = 5;
+   console.log(addNumbers(a, b));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
